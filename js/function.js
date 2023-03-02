36ad64f83d83217d4ce3db91b7da818c64c9d894
Micaela <mica123c@gamil.com>
2023-03-02 14:31:09 -0300

    Mi primer commit

---
+++ js/function.js
@@ -0,0 +1,57 @@
+function mostrarNombre() {
+  var texto = document.getElementById("texto");
+  if (texto.style.display === "none") {
+    texto.style.display = "block";
+  } else {
+    texto.style.display = "none";
+  }
+}
+
+function mostrarMail (){
+  var texto1 = document.getElementById("mail");
+  if (texto1.style.display=== "none"){
+  texto1.style.display = "block";
+  } else {
+    texto1.style.display = "none";
+  }
+}
+function mostrarFecha(){
+  var texto2 = document.getElementById("fecha");
+  if(texto2.style.display==="none"){
+  texto2.style.display = "block";
+  } else{
+    texto2.style.display = "none";
+  }
+}
+function mostrarDireccion() {
+  var texto3 = document.getElementById("direccion");
+  if (texto3.style.display === "none") {
+    texto3.style.display = "block";
+  } else {
+    texto3.style.display = "none";
+  }
+}
+function mostrarTelefono() {
+  var texto4 = document.getElementById("telefono");
+  if (texto4.style.display === "none") {
+    texto4.style.display = "block";
+  } else {
+    texto4.style.display = "none";
+  }
+}
+function mostrarTelefono() {
+  var texto5 = document.getElementById("telefono");
+  if (texto5.style.display === "none") {
+    texto5.style.display = "block";
+  } else {
+    texto5.style.display = "none";
+  }
+}
+function mostrarConstraseña() {
+  var texto6 = document.getElementById("contraseña");
+  if (texto6.style.display === "none") {
+    texto6.style.display = "block";
+  } else {
+    texto6.style.display = "none";
+  }
+}
